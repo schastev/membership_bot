85dcbcad66e9667095a943d856a54948730edddc
schastev <62057364+schastev@users.noreply.github.com>
2023-06-30 00:30:12 +0500

    init commit

---
+++ db.py
@@ -0,0 +1,86 @@
+from datetime import date, timedelta
+from typing import Union
+
+
+def check_membership_validity(membership: "Membership") -> bool:
+    return membership.current_amount > 0 and membership.expiry_date > date.today()
+
+
+class DataBase:  # witness my brilliant stub and marvel at its ingeniousness
+    memberships: list
+
+    def __init__(self) -> None:
+        self.memberships = []
+
+    def add_membership(self, membership: "Membership") -> None:
+        self.memberships.append(membership)
+
+    def remove_membership(self, membership: "Membership") -> None:
+        self.memberships.remove(membership)
+
+    def update_membership(self, old_membership: "Membership", new_membership: "Membership"):
+        # todo will need to replace this with select by id once I move on to an actual database
+        index = self.memberships.index(old_membership)
+        self.memberships.pop(index)
+        self.memberships.append(new_membership)
+
+
+class Membership:
+    member_nickname: str
+    purchase_date: date
+    activation_date: Union[date, None] = None
+    expiry_date: Union[date, None] = None
+    original_expiry_date: Union[date, None] = None
+    frozen: bool
+    freeze_date: Union[date, None] = None
+    unfreeze_date: Union[date, None] = None
+    total_amount: int
+    current_amount: int
+
+    def __init__(self, member_nickname: str, total_amount: int) -> None:
+        self.member_nickname = member_nickname
+        self.total_amount = total_amount
+        self.current_amount = total_amount
+        self.purchase_date = date.today()
+
+    def freeze(self, days: int) -> None:
+        self._freeze_with_date(date.today(), days=days)
+
+    def unfreeze(self) -> None:
+        self._unfreeze_with_date(unfreeze_date=date.today())
+
+    def _freeze_with_date(self, freeze_date: date, days: int) -> None:
+        if days > 14:
+            raise ValueError("Заморозить абонемент можно не более чем на две недели.")
+        if self.unfreeze_date:
+            raise ValueError("Заморозить один абонемент можно только один раз.")
+        self.frozen = True
+        self.freeze_date = freeze_date
+        self.unfreeze_date = freeze_date + timedelta(days=days)
+        self.expiry_date += self.unfreeze_date - self.freeze_date
+
+    def _unfreeze_with_date(self, unfreeze_date: date) -> None:
+        if not self.frozen:
+            raise ValueError("Указанный абонемент не был заморожен.")
+        self.unfreeze_date = unfreeze_date
+        self.frozen = False
+        new_expiry_date = self.original_expiry_date + (unfreeze_date - self.freeze_date)
+        if new_expiry_date - self.original_expiry_date > timedelta(days=14):
+            raise ValueError("Заморозить абонемент можно не более чем на две недели.")
+        if (new_expiry_date - self.original_expiry_date).days < 0:
+            raise ValueError("Отрицательная продолжительность периода заморозки.")
+        if self.expiry_date != new_expiry_date:
+            self.expiry_date = new_expiry_date
+        self.freeze_date = None
+
+    def subtract(self) -> None:
+        if not self.activation_date:
+            self._activate(date.today())
+        if not check_membership_validity(self):
+            raise ValueError("Абонемент полностью использован или истек срок его действия.")
+        self.current_amount -= self.current_amount
+
+    def _activate(self, activation_date: date) -> None:
+        self.activation_date = activation_date
+        self.expiry_date = activation_date + timedelta(days=30)
+        self.original_expiry_date = activation_date + timedelta(days=30)
---
+++ test_project.py
@@ -0,0 +1,133 @@
+from copy import deepcopy
+from datetime import date, timedelta
+
+import pytest
+
+from db import DataBase, Membership, check_membership_validity
+
+
+# TODO move helper functions to the helper file
+def mb_activate(mb: Membership, activation_date: date = date.today()):
+    mb._activate(activation_date)
+    assert mb.expiry_date == activation_date + timedelta(days=30)
+
+
+def mb_freeze(mb: Membership, freeze_date: date, freeze_duration: int):
+    mb._freeze_with_date(freeze_date=freeze_date, days=freeze_duration)
+    assert mb.expiry_date == mb.original_expiry_date + timedelta(days=freeze_duration)
+    assert mb.unfreeze_date == freeze_date + timedelta(days=freeze_duration)
+
+
+def mb_unfreeze(mb: Membership, unfreeze_date: date, new_expiry_date: date):
+    mb._unfreeze_with_date(unfreeze_date=unfreeze_date)
+    assert mb.expiry_date == new_expiry_date
+
+
+def db_add(db: DataBase, mb: Membership):
+    deepcopy_one = deepcopy(mb)
+    old_len = len(db.memberships)
+    db.add_membership(deepcopy_one)
+    assert len(db.memberships) == old_len + 1
+    return deepcopy_one
+
+
+def test_membership_freezing_and_unfreezing_delays_expiry():
+    mb = Membership(member_nickname="123", total_amount=4)
+    activation_date = date.today()
+    mb_activate(mb, activation_date)
+
+    freeze_date = activation_date - timedelta(days=5)
+    freeze_duration = 7
+    unfreeze_date = freeze_date + timedelta(days=freeze_duration)
+    mb_freeze(mb, freeze_date, freeze_duration)
+
+    new_expiry_date = mb.original_expiry_date + timedelta(days=freeze_duration)
+    mb_unfreeze(mb, unfreeze_date, new_expiry_date)
+
+
+def test_membership_cannot_freeze_two_times():
+    mb = Membership(member_nickname="123", total_amount=4)
+    activation_date = date.today()
+    mb_activate(mb, activation_date)
+
+    freeze_date = activation_date - timedelta(days=5)
+    freeze_duration = 7
+    mb_freeze(mb, freeze_date, freeze_duration)
+
+    unfreeze_date = freeze_date + timedelta(days=freeze_duration)
+    new_expiry_date = mb.original_expiry_date + timedelta(days=freeze_duration)
+    mb_unfreeze(mb, unfreeze_date, new_expiry_date)
+    with pytest.raises(ValueError):
+        mb_freeze(mb, freeze_date, freeze_duration)
+
+
+@pytest.mark.parametrize("delta", [2, -2], ids=["later than planned", "earlier than planned"])
+def test_membership_can_unfreeze_earlier_or_later_than_planned(delta):
+    mb = Membership(member_nickname="123", total_amount=4)
+    activation_date = date.today()
+    mb_activate(mb, activation_date)
+
+    freeze_date = activation_date + timedelta(days=5)
+    planned_freeze_duration = 7
+    mb_freeze(mb, freeze_date, planned_freeze_duration)
+
+    actual_freeze_duration = planned_freeze_duration + delta
+    unfreeze_date = freeze_date + timedelta(days=actual_freeze_duration)
+    new_expiry_date = mb.original_expiry_date + timedelta(days=actual_freeze_duration)
+    mb_unfreeze(mb, unfreeze_date=unfreeze_date, new_expiry_date=new_expiry_date)
+
+
+@pytest.mark.parametrize("delta", [15, -15], ids=["over two weeks", "negative freeze period"])
+def test_membership_cannot_unfreeze_to_make_freeze_period_invalid(delta):
+    mb = Membership(member_nickname="123", total_amount=4)
+    activation_date = date.today()
+    mb_activate(mb, activation_date)
+
+    freeze_date = activation_date - timedelta(days=5)
+    planned_freeze_duration = 7
+    mb_freeze(mb, freeze_date, planned_freeze_duration)
+
+    actual_freeze_duration = planned_freeze_duration + delta
+    unfreeze_date = freeze_date + timedelta(days=actual_freeze_duration)
+    new_expiry_date = mb.original_expiry_date + timedelta(days=actual_freeze_duration)
+    with pytest.raises(expected_exception=ValueError):
+        mb_unfreeze(mb, unfreeze_date=unfreeze_date, new_expiry_date=new_expiry_date)
+
+
+def test_membership_cannot_freeze_for_over_two_weeks():
+    mb = Membership(member_nickname="123", total_amount=4)
+    activation_date = date.today()
+    mb_activate(mb, activation_date)
+
+    freeze_date = activation_date - timedelta(days=5)
+    planned_freeze_duration = 15
+    with pytest.raises(expected_exception=ValueError):
+        mb_freeze(mb, freeze_date, planned_freeze_duration)
+
+
+def test_database_update():
+    mb_one = Membership(member_nickname="123", total_amount=8)
+    mb_two = Membership(member_nickname="234", total_amount=8)
+    db = DataBase()
+    deepcopy_one = db_add(db, mb_one)
+    db_add(db, mb_two)
+
+    mb_activate(mb_one, date.today())
+    db.update_membership(old_membership=deepcopy_one, new_membership=mb_one)
+    assert db.memberships[1] == mb_one
+    assert len(db.memberships) == 2
+
+
+def test_membership_validity():
+    mb_valid = Membership(member_nickname="123", total_amount=4)
+    mb_activate(mb_valid)
+    assert check_membership_validity(mb_valid)
+
+    mb_expired = Membership(member_nickname="123", total_amount=4)
+    mb_activate(mb_expired, activation_date=date.today() - timedelta(days=31))
+    assert not check_membership_validity(mb_expired)
+
+    mb_used_up = Membership(member_nickname="123", total_amount=1)
+    mb_activate(mb_used_up)
+    mb_used_up.subtract()
+    assert not check_membership_validity(mb_used_up)
