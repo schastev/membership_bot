3f597bc0871e54fd101d5f95f445f207ea0fff55
schastev <62057364+schastev@users.noreply.github.com>
2024-02-11 12:58:09 +0500

    some more cleanup

--- src/db_calls/mb_for_member.py
+++ src/db_calls/mb_for_member.py
@@ -5,18 +5,18 @@ from sqlalchemy.orm import Session
 
 from src.model.membership import Membership
 from src.model.request import MembershipRequest
-from src.db_calls.database import ENGINE, get_memberships_by_tg_id
+from src.db_calls import database
 
 
 def view_memberships_by_user_id(tg_id: int) -> List[Membership]:
-    with Session(ENGINE) as session:
-        memberships = session.scalars(get_memberships_by_tg_id(tg_id=tg_id)).all()
+    with Session(database.ENGINE) as session:
+        memberships = session.scalars(database.get_memberships_by_tg_id(tg_id=tg_id)).all()
     return memberships
 
 
 def get_active_membership_by_user_id(tg_id: int) -> Union[Membership, None]:
-    with Session(ENGINE) as session:
-        memberships = session.scalars(get_memberships_by_tg_id(tg_id=tg_id)).all()
+    with Session(database.ENGINE) as session:
+        memberships = session.scalars(database.get_memberships_by_tg_id(tg_id=tg_id)).all()
     active_mb = [mb for mb in memberships if mb.is_valid()]
     if len(active_mb) == 0:
         return None
@@ -24,11 +24,11 @@ def get_active_membership_by_user_id(tg_id: int) -> Union[Membership, None]:
 
 
 def request_to_add_membership(tg_id: int, chat_id: int) -> MembershipRequest:
-    with Session(ENGINE) as session:
+    with Session(database.ENGINE) as session:
         request = MembershipRequest(tg_id=tg_id, chat_id=chat_id)
         session.add(request)
         session.commit()
-    with Session(ENGINE) as session:
+    with Session(database.ENGINE) as session:
         query = select(MembershipRequest).where(MembershipRequest.tg_id == tg_id)
         request = session.scalars(query).first()
     assert request is not None
