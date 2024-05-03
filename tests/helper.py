fdcc8a18723723e65238b98790e2218593b2695b
schastev <62057364+schastev@users.noreply.github.com>
2024-05-04 01:31:17 +0500

    select random locale

--- tests/helper.py
+++ tests/helper.py
@@ -1,7 +1,14 @@
+from random import Random
 from typing import List
+
+import config_reader
 
 
 def extract_keyboard_entries(keyboard) -> List[str]:
     result = []
     [result.extend(r) for r in keyboard]
     return [button.text.replace("_button", "") for button in result]
+
+
+def get_random_locale() -> str:
+    return Random().choice(config_reader.config.locales)
--- tests/test_membership_handlers.py
+++ tests/test_membership_handlers.py
@@ -9,19 +9,24 @@ from src.routers.mb_for_admin import process_membership
 from src.utils import translation
 from src.utils.bot_helpers import IsAdmin
 from src.utils.callback_factories import MBRequestValueCallbackFactory
-
+from src.utils.translation import locale
+from tests.helper import get_random_locale
 
 _ = translation.i18n.gettext
 
 
 @pytest.mark.asyncio
 async def test_process_membership():
+    random_locale = get_random_locale()
     request = request_to_add_membership(tg_id=USER.get("id"), chat_id=CHAT.get("id"))
-    requester = MockedRequester(
-        CallbackQueryHandler(
-            process_membership, MBRequestValueCallbackFactory.filter(), IsAdmin()
-        )
+    handler = CallbackQueryHandler(
+        process_membership,
+        MBRequestValueCallbackFactory.filter(),
+        IsAdmin(),
+        state_data={"locale": random_locale},
+        dp_middlewares=[locale],
     )
+    requester = MockedRequester(handler)
     callback_query = CALLBACK_QUERY.as_object(
         data=MBRequestValueCallbackFactory(
             member_tg_id=USER.get("id"),
@@ -37,6 +42,8 @@ async def test_process_membership():
         [message.text for message in calls.send_message.fetchall()]
     )
     assert answer_messages == [
-        _("ADD_MEMBERSHIP_ok_admin").format(mb_value=10, member_name=USER.get("username")),
-        _("ADD_MEMBERSHIP_ok_member").format(mb_value=10),
+        _("ADD_MEMBERSHIP_ok_admin", locale=random_locale).format(
+            mb_value=10, member_name=USER.get("username")
+        ),
+        _("ADD_MEMBERSHIP_ok_member", locale=random_locale).format(mb_value=10),
     ]
