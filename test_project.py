b68b58de72c02dd8e218d4e097d5bd04f1ba814a
schastev <62057364+schastev@users.noreply.github.com>
2024-05-10 18:05:33 +0500

    add allure steps

--- test_project.py
+++ test_project.py
@@ -1,6 +1,7 @@
 import datetime
 from random import Random
 
+import allure
 import pytest
 
 from config_reader import GlobalSettings
@@ -14,28 +15,33 @@ _ = GlobalSettings().i18n.gettext
 
 
 def test_main_buttons_not_registered():
-    user_state = UserState(tg_id=0)
-    is_admin = Random().choice([True, False])
-    user_state.is_admin = is_admin
-    keyboard = main_menu(user_id=0, user_state=user_state)
-    buttons = extract_keyboard_entries(keyboard)
-    expected = [Action.REGISTER]
-    if is_admin:
-        expected.extend(MenuButtons.ADMIN)
-    assert sorted(buttons) == sorted([_(f"{ex}{Modifier.BUTTON}") for ex in expected])
+    with allure.step("Assemble"):
+        user_state = UserState(tg_id=0)
+        is_admin = Random().choice([True, False])
+        user_state.is_admin = is_admin
+    with allure.step("Act"):
+        keyboard = main_menu(user_id=0, user_state=user_state)
+        buttons = extract_keyboard_entries(keyboard)
+    with allure.step("Assert"):
+        expected = [Action.REGISTER]
+        if user_state.is_admin:
+            expected.extend(MenuButtons.ADMIN)
+        assert sorted(buttons) == sorted([_(f"{ex}{Modifier.BUTTON}") for ex in expected])
 
 
 def test_main_buttons_registered():
-    user_state = UserState(tg_id=0)
-    is_admin = Random().choice([True, False])
-    user_state.is_admin = is_admin
-    user_state.is_registered = True
-    keyboard = main_menu(user_id=0, user_state=user_state)
-    buttons = extract_keyboard_entries(keyboard)
-    expected = [Action.CHANGE_SETTINGS, Action.ADD_MEMBERSHIP]
-    if is_admin:
-        expected.extend(MenuButtons.ADMIN)
-    assert sorted(buttons) == sorted([_(f"{ex}{Modifier.BUTTON}") for ex in expected])
+    with allure.step("Assemble"):
+        user_state = UserState(tg_id=0)
+        user_state.is_admin = Random().choice([True, False])
+        user_state.is_registered = True
+    with allure.step("Act"):
+        keyboard = main_menu(user_id=0, user_state=user_state)
+        buttons = extract_keyboard_entries(keyboard)
+    with allure.step("Assert"):
+        expected = [Action.CHANGE_SETTINGS, Action.ADD_MEMBERSHIP]
+        if user_state.is_admin:
+            expected.extend(MenuButtons.ADMIN)
+        assert sorted(buttons) == sorted([_(f"{ex}{Modifier.BUTTON}") for ex in expected])
 
 
 @pytest.mark.parametrize(
@@ -50,20 +56,20 @@ def test_main_buttons_registered():
     ],
 )
 def test_main_buttons_with_and_without_active_membership(has_active_mb, expected):
-    user_state = UserState(tg_id=0)
-    is_admin = Random().choice([True, False])
-    user_state.is_admin = is_admin
-    (
-        user_state.is_registered,
-        user_state.has_memberships,
-        user_state.has_usable_membership,
-    ) = True, True, has_active_mb
-    keyboard = main_menu(user_id=0, user_state=user_state)
-    buttons = extract_keyboard_entries(keyboard)
-    expected = [Action.CHANGE_SETTINGS, Action.VIEW_ALL_MEMBERSHIPS, *expected]
-    if is_admin:
-        expected.extend(MenuButtons.ADMIN)
-    assert sorted(buttons) == sorted([_(f"{ex}{Modifier.BUTTON}") for ex in expected])
+    with allure.step("Assemble"):
+        user_state = UserState(tg_id=0)
+        user_state.is_admin = Random().choice([True, False])
+        user_state.is_registered = True
+        user_state.has_memberships = True
+        user_state.has_usable_membership = has_active_mb
+    with allure.step("Act"):
+        keyboard = main_menu(user_id=0, user_state=user_state)
+        buttons = extract_keyboard_entries(keyboard)
+    with allure.step("Assert"):
+        expected = [Action.CHANGE_SETTINGS, Action.VIEW_ALL_MEMBERSHIPS, *expected]
+        if user_state.is_admin:
+            expected.extend(MenuButtons.ADMIN)
+        assert sorted(buttons) == sorted([_(f"{ex}{Modifier.BUTTON}") for ex in expected])
 
 
 @pytest.mark.parametrize(
@@ -76,37 +82,36 @@ def test_main_buttons_with_and_without_active_membership(has_active_mb, expected
     ],
 )
 def test_main_buttons_active_membership_states(frozen, unfrozen, expected):
-    active_mb = Membership(tg_id=1, total_amount=8)
-    active_mb._activate(
-        activation_date=datetime.date.today() - datetime.timedelta(days=1)
-    )
-    if frozen:
-        active_mb.freeze(
-            freeze_date=datetime.date.today() - datetime.timedelta(days=1), days=10
+    with allure.step("Assemble"):
+        active_mb = Membership(tg_id=1, total_amount=8)
+        active_mb._activate(
+            activation_date=datetime.date.today() - datetime.timedelta(days=1)
         )
-    if unfrozen:
-        active_mb.unfreeze()
-    user_state = UserState(tg_id=0, active_mb=active_mb)
-    is_admin = Random().choice([True, False])
-    has_attendances = Random().choice([True, False])
-    (
-        user_state.is_admin,
-        user_state.is_registered,
-        user_state.has_usable_membership,
-        user_state.has_memberships,
-        user_state.has_attendances,
-    ) = is_admin, True, True, True, has_attendances
-    keyboard = main_menu(user_id=0, user_state=user_state)
-    buttons = extract_keyboard_entries(keyboard)
-    expected = [
-        Action.CHANGE_SETTINGS,
-        Action.VIEW_ACTIVE_MEMBERSHIP,
-        Action.CHECK_IN,
-        Action.VIEW_ALL_MEMBERSHIPS,
-        *expected,
-    ]
-    if is_admin:
-        expected.extend(MenuButtons.ADMIN)
-    if has_attendances:
-        expected.append(Action.VIEW_ATTENDANCES)
-    assert sorted(buttons) == sorted([_(f"{ex}{Modifier.BUTTON}") for ex in expected])
+        if frozen:
+            active_mb.freeze(
+                freeze_date=datetime.date.today() - datetime.timedelta(days=1), days=10
+            )
+        if unfrozen:
+            active_mb.unfreeze()
+        user_state = UserState(tg_id=0, active_mb=active_mb)
+        user_state.is_admin = Random().choice([True, False])
+        user_state.is_registered = True
+        user_state.has_usable_membership = True
+        user_state.has_memberships = True
+        user_state.has_attendances = Random().choice([True, False])
+    with allure.step("Act"):
+        keyboard = main_menu(user_id=0, user_state=user_state)
+        buttons = extract_keyboard_entries(keyboard)
+    with allure.step("Assert"):
+        expected = [
+            Action.CHANGE_SETTINGS,
+            Action.VIEW_ACTIVE_MEMBERSHIP,
+            Action.CHECK_IN,
+            Action.VIEW_ALL_MEMBERSHIPS,
+            *expected,
+        ]
+        if user_state.is_admin:
+            expected.extend(MenuButtons.ADMIN)
+        if user_state.has_attendances:
+            expected.append(Action.VIEW_ATTENDANCES)
+        assert sorted(buttons) == sorted([_(f"{ex}{Modifier.BUTTON}") for ex in expected])
--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 from aiogram_tests.types.dataset import CHAT
 
@@ -11,181 +12,206 @@ _ = GlobalSettings().i18n.gettext
 
 @pytest.mark.asyncio
 async def test_process_membership(db_calls, routers, get_user):
-    random_locale = helper.get_random_locale()
-    user, mb = get_user(with_membership=False, locale=random_locale)
-    request = db_calls.member.request_to_add_membership(
-        tg_id=user.tg_id, chat_id=CHAT.get("id")
-    )
-    value = 10
-    answer = await helper.send_mocked_message_with_factory_data_callback_admin(
-        locale=random_locale,
-        handler_to_mock=routers.mb_for_admin.process_membership,
-        factory=MBRequestValueCallbackFactory,
-        member_tg_id=user.tg_id,
-        member_name=user.name,
-        value=value,
-        chat_id=CHAT.get("id"),
-        id=request.id,
-    )
-    assert sorted([a.text for a in answer]) == sorted(
-        [
-            _("ADD_MEMBERSHIP_ok_admin", locale=random_locale).format(
-                mb_value=value, member_name=user.name
-            ),
-            _("ADD_MEMBERSHIP_ok_member", locale=random_locale).format(mb_value=value),
-        ]
-    )
+    with allure.step("Assemble"):
+        random_locale = helper.get_random_locale()
+        user, mb = get_user(with_membership=False, locale=random_locale)
+        request = db_calls.member.request_to_add_membership(
+            tg_id=user.tg_id, chat_id=CHAT.get("id")
+        )
+        value = 10
+    with allure.step("Process membership request"):
+        answer = await helper.send_mocked_message_with_factory_data_callback_admin(
+            locale=random_locale,
+            handler_to_mock=routers.mb_for_admin.process_membership,
+            factory=MBRequestValueCallbackFactory,
+            member_tg_id=user.tg_id,
+            member_name=user.name,
+            value=value,
+            chat_id=CHAT.get("id"),
+            id=request.id,
+        )
+    with allure.step("Check replies"):
+        assert sorted([a.text for a in answer]) == sorted(
+            [
+                _("ADD_MEMBERSHIP_ok_admin", locale=random_locale).format(
+                    mb_value=value, member_name=user.name
+                ),
+                _("ADD_MEMBERSHIP_ok_member", locale=random_locale).format(mb_value=value),
+            ]
+        )
 
 
 @pytest.mark.asyncio
 async def test_poll_for_mb_add_request(db_calls, routers, get_user):
-    random_locale = helper.get_random_locale()
-    test_function = routers.mb_for_admin.poll_for_mb_add_request
-    action = Action.ADD_MEMBERSHIP
-    pending_message = _("pending_membership", locale=random_locale)
-    button_name = _("ADD_MEMBERSHIP_button", locale=random_locale)
-    user, mb = get_user(with_membership=False, locale=random_locale)
-    polling_message = _("polling", locale=random_locale).format(
-        request_type_string=pending_message,
-        timeout_seconds=GlobalSettings().config.polling_timeout_seconds,
-        button_name=button_name,
-    )
-    first_answer = await helper.send_mocked_message_with_text_data_callback_admin(
-        locale=random_locale, action=action, handler_to_mock=test_function
-    )
-    assert sorted([a.text for a in first_answer]) == sorted(
-        [
-            polling_message,
-            _("polling_timeout", locale=random_locale).format(button_name=button_name),
-        ]
-    )
-    request = db_calls.member.request_to_add_membership(
-        tg_id=user.tg_id, chat_id=CHAT.get("id")
-    )
-    second_answer = await helper.send_mocked_message_with_text_data_callback_admin(
-        locale=random_locale, action=action, handler_to_mock=test_function
-    )
-    assert sorted([a.text for a in second_answer]) == sorted(
-        [
-            polling_message,
-            _("pending_list", locale=random_locale).format(
-                request_type=pending_message
-            ),
-        ]
-    )
-    kb = helper.extract_keyboard_entries(second_answer[1].reply_markup)
-    assert len(kb) == 1
-    assert kb[0] == f"{user.name}: {int(user.phone)}"
-    db_calls.admin.delete_request(request_id=request.id)
+    with allure.step("Assemble"):
+        random_locale = helper.get_random_locale()
+        test_function = routers.mb_for_admin.poll_for_mb_add_request
+        action = Action.ADD_MEMBERSHIP
+        pending_message = _("pending_membership", locale=random_locale)
+        button_name = _("ADD_MEMBERSHIP_button", locale=random_locale)
+        user, mb = get_user(with_membership=False, locale=random_locale)
+        polling_message = _("polling", locale=random_locale).format(
+            request_type_string=pending_message,
+            timeout_seconds=GlobalSettings().config.polling_timeout_seconds,
+            button_name=button_name,
+        )
+    with allure.step("Poll for membership requests"):
+        first_answer = await helper.send_mocked_message_with_text_data_callback_admin(
+            locale=random_locale, action=action, handler_to_mock=test_function
+        )
+    with allure.step("Check there is no requests"):
+        assert sorted([a.text for a in first_answer]) == sorted(
+            [
+                polling_message,
+                _("polling_timeout", locale=random_locale).format(button_name=button_name),
+            ]
+        )
+    with allure.step("Add a membership request"):
+        request = db_calls.member.request_to_add_membership(
+            tg_id=user.tg_id, chat_id=CHAT.get("id")
+        )
+    with allure.step("Poll for membership requests"):
+        second_answer = await helper.send_mocked_message_with_text_data_callback_admin(
+            locale=random_locale, action=action, handler_to_mock=test_function
+        )
+    with allure.step("Check a membership request appeared"):
+        assert sorted([a.text for a in second_answer]) == sorted(
+            [
+                polling_message,
+                _("pending_list", locale=random_locale).format(
+                    request_type=pending_message
+                ),
+            ]
+        )
+        kb = helper.extract_keyboard_entries(second_answer[1].reply_markup)
+        assert len(kb) == 1
+        assert kb[0] == f"{user.name}: {int(user.phone)}"
+        db_calls.admin.delete_request(request_id=request.id)
 
 
 @pytest.mark.asyncio
 async def test_poll_for_mb_freeze_request(db_calls, routers, get_user):
-    random_locale = helper.get_random_locale()
-    test_function = routers.mb_for_admin.poll_for_mb_freeze_request
-    action = Action.FREEZE_MEMBERSHIP
-    pending_message = _("pending_freeze", locale=random_locale)
-    button_name = _("FREEZE_MEMBERSHIP_button", locale=random_locale)
-    user, membership = get_user(with_membership=True, locale=random_locale)
-    duration = 10
-    polling_message = _("polling", locale=random_locale).format(
-        request_type_string=pending_message,
-        timeout_seconds=GlobalSettings().config.polling_timeout_seconds,
-        button_name=button_name,
-    )
-    first_answer = await helper.send_mocked_message_with_text_data_callback_admin(
-        locale=random_locale, action=action, handler_to_mock=test_function
-    )
-    assert sorted([a.text for a in first_answer]) == sorted(
-        [
-            polling_message,
-            _("polling_timeout", locale=random_locale).format(button_name=button_name),
-        ]
-    )
-    db_calls.member.request_to_freeze_membership(
-        tg_id=user.tg_id, chat_id=CHAT.get("id"), mb_id=membership.id, duration=duration
-    )
-    second_answer = await helper.send_mocked_message_with_text_data_callback_admin(
-        locale=random_locale, action=action, handler_to_mock=test_function
-    )
-    assert sorted([a.text for a in second_answer]) == sorted(
-        [
-            polling_message,
-            _("pending_list", locale=random_locale).format(
-                request_type=pending_message
-            ),
-        ]
-    )
-    kb = helper.extract_keyboard_entries(second_answer[1].reply_markup)
-    assert len(kb) == 1
-    assert kb[0] == f"{user.name}: {int(user.phone)}: {duration}"
+    with allure.step("Assemble"):
+        random_locale = helper.get_random_locale()
+        test_function = routers.mb_for_admin.poll_for_mb_freeze_request
+        action = Action.FREEZE_MEMBERSHIP
+        pending_message = _("pending_freeze", locale=random_locale)
+        button_name = _("FREEZE_MEMBERSHIP_button", locale=random_locale)
+        user, membership = get_user(with_membership=True, locale=random_locale)
+        duration = 10
+        polling_message = _("polling", locale=random_locale).format(
+            request_type_string=pending_message,
+            timeout_seconds=GlobalSettings().config.polling_timeout_seconds,
+            button_name=button_name,
+        )
+    with allure.step("Poll for freeze requests"):
+        first_answer = await helper.send_mocked_message_with_text_data_callback_admin(
+            locale=random_locale, action=action, handler_to_mock=test_function
+        )
+    with allure.step("Check there is no requests"):
+        assert sorted([a.text for a in first_answer]) == sorted(
+            [
+                polling_message,
+                _("polling_timeout", locale=random_locale).format(button_name=button_name),
+            ]
+        )
+    with allure.step("Add a freeze request"):
+        db_calls.member.request_to_freeze_membership(
+            tg_id=user.tg_id, chat_id=CHAT.get("id"), mb_id=membership.id, duration=duration
+        )
+    with allure.step("Poll for freeze requests"):
+        second_answer = await helper.send_mocked_message_with_text_data_callback_admin(
+            locale=random_locale, action=action, handler_to_mock=test_function
+        )
+    with allure.step("Check a freeze request appeared"):
+        assert sorted([a.text for a in second_answer]) == sorted(
+            [
+                polling_message,
+                _("pending_list", locale=random_locale).format(
+                    request_type=pending_message
+                ),
+            ]
+        )
+        kb = helper.extract_keyboard_entries(second_answer[1].reply_markup)
+        assert len(kb) == 1
+        assert kb[0] == f"{user.name}: {int(user.phone)}: {duration}"
 
 
 @pytest.mark.asyncio
 async def test_poll_for_check_in_request(db_calls, routers, get_user):
-    random_locale = helper.get_random_locale()
-    test_function = routers.att_for_admin.poll_for_check_in_request
-    action = Action.CHECK_IN
-    pending_message = _("pending_attendance", locale=random_locale)
-    button_name = _("CHECK_IN_button", locale=random_locale)
-
-    user, membership = get_user(with_membership=True, locale=random_locale)
-    polling_message = _("polling", locale=random_locale).format(
-        request_type_string=pending_message,
-        timeout_seconds=GlobalSettings().config.polling_timeout_seconds,
-        button_name=button_name,
-    )
-    first_answer = await helper.send_mocked_message_with_text_data_callback_admin(
-        locale=random_locale, action=action, handler_to_mock=test_function
-    )
-    assert sorted([a.text for a in first_answer]) == sorted(
-        [
-            polling_message,
-            _("polling_timeout", locale=random_locale).format(button_name=button_name),
-        ]
-    )
-    db_calls.member.request_to_add_attendance(
-        tg_id=user.tg_id, chat_id=CHAT.get("id"), mb_id=membership.id
-    )
-    second_answer = await helper.send_mocked_message_with_text_data_callback_admin(
-        locale=random_locale, action=action, handler_to_mock=test_function
-    )
-    assert sorted([a.text for a in second_answer]) == sorted(
-        [
-            polling_message,
-            _("pending_list", locale=random_locale).format(
-                request_type=pending_message
-            ),
-        ]
-    )
-    kb = helper.extract_keyboard_entries(second_answer[1].reply_markup)
-    assert len(kb) == 1
-    assert kb[0] == f"{user.name}: {int(user.phone)}"
+    with allure.step("Assemble"):
+        random_locale = helper.get_random_locale()
+        test_function = routers.att_for_admin.poll_for_check_in_request
+        action = Action.CHECK_IN
+        pending_message = _("pending_attendance", locale=random_locale)
+        button_name = _("CHECK_IN_button", locale=random_locale)
+        user, membership = get_user(with_membership=True, locale=random_locale)
+        polling_message = _("polling", locale=random_locale).format(
+            request_type_string=pending_message,
+            timeout_seconds=GlobalSettings().config.polling_timeout_seconds,
+            button_name=button_name,
+        )
+    with allure.step("Poll for check-in requests"):
+        first_answer = await helper.send_mocked_message_with_text_data_callback_admin(
+            locale=random_locale, action=action, handler_to_mock=test_function
+        )
+    with allure.step("Check there is no requests"):
+        assert sorted([a.text for a in first_answer]) == sorted(
+            [
+                polling_message,
+                _("polling_timeout", locale=random_locale).format(button_name=button_name),
+            ]
+        )
+    with allure.step("Add a check-in request"):
+        db_calls.member.request_to_add_attendance(
+            tg_id=user.tg_id, chat_id=CHAT.get("id"), mb_id=membership.id
+        )
+    with allure.step("Poll for check-in requests"):
+        second_answer = await helper.send_mocked_message_with_text_data_callback_admin(
+            locale=random_locale, action=action, handler_to_mock=test_function
+        )
+    with allure.step("Check a check-in request appeared"):
+        assert sorted([a.text for a in second_answer]) == sorted(
+            [
+                polling_message,
+                _("pending_list", locale=random_locale).format(
+                    request_type=pending_message
+                ),
+            ]
+        )
+        kb = helper.extract_keyboard_entries(second_answer[1].reply_markup)
+        assert len(kb) == 1
+        assert kb[0] == f"{user.name}: {int(user.phone)}"
 
 
 @pytest.mark.asyncio
 async def test_locale_handler(routers):
-    for loc in GlobalSettings().config.locales:
-        answer = await helper.send_mocked_message_with_text_data_callback_non_admin(
-            handler_to_mock=routers.misc.locale_handler,
-            callback_data=loc,
-        )
-        answer_message = answer[0]
-        assert answer_message.text == _("greeting", locale=loc).format(
-            company_name=GlobalSettings().config.company_name
-        )
+    with allure.step("Assemble"):
+        for loc in GlobalSettings().config.locales:
+            with allure.step("Trigger locale callback"):
+                answer = await helper.send_mocked_message_with_text_data_callback_non_admin(
+                    handler_to_mock=routers.misc.locale_handler,
+                    callback_data=loc,
+                )
+            with allure.step("Check answer"):
+                answer_message = answer[0]
+                assert answer_message.text == _("greeting", locale=loc).format(
+                    company_name=GlobalSettings().config.company_name
+                )
 
 
 @pytest.mark.asyncio
 async def test_start_handler(routers):
-    answer = await helper.send_mocked_message_with_text_data_message_non_admin(
-        handler_to_mock=routers.misc.start_handler,
-        message_text="start",
-    )
-    answer_message = answer[0]
-    assert answer_message.text == "\n".join(
-        [
-            _("first_greeting", locale=locale)
-            for locale in GlobalSettings().config.locales
-        ]
-    )
+    with allure.step("Send a start command"):
+        answer = await helper.send_mocked_message_with_text_data_message_non_admin(
+            handler_to_mock=routers.misc.start_handler,
+            message_text="start",
+        )
+    with allure.step("Check answer"):
+        answer_message = answer[0]
+        assert answer_message.text == "\n".join(
+            [
+                _("first_greeting", locale=locale)
+                for locale in GlobalSettings().config.locales
+            ]
+        )
